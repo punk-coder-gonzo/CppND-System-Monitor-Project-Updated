4bc37e986a1011130d045378acae9a1c791d247a
Mario Witter <mario.witter@gmx.de>
2020-07-07 09:26:22 +0200

    implemented Format::Time

--- test/test_monitor.cpp
+++ test/test_monitor.cpp
@@ -6,6 +6,8 @@
 TEST(MonitorTest, TestTimeFormat){
     // INPUT: Long int measuring seconds
     // OUTPUT: HH:MM:SS
+    // Extention: D days, HH:MM:SS for very long uptimes
     EXPECT_EQ(Format::Time(10),"00:00:10");
-    EXPECT_EQ(Format::Time(1594063617), "19:26:57");
+    EXPECT_EQ(Format::Time(45296), "12:34:56");
+    EXPECT_EQ(Format::Time(1594063617), "18449 days, 19:26:57");
 }
